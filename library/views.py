a8004d97db3112ecb825034144023150165c69ad
Vagner Elena <dipnoe@gmail.com>
2023-11-24 22:51:04 +0700

    feature: add CRUD for book

--- library/views.py
+++ library/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
+from library.models import Book
+from library.serializers import BookSerializer
+
 
 # Create your views here.
+class BookListAPIView(ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookRetrieveAPIView(RetrieveAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookCreateAPIView(CreateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookUpdateAPIView(UpdateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookDestroyAPIView(DestroyAPIView):
+    queryset = Book.objects.all()
