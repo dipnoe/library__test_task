96453f16e789ad5ece5fd252bc609b8808b8ea9b
Vagner Elena <dipnoe@gmail.com>
2023-11-24 22:12:56 +0700

    feature(model): add Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название')
+    autor = models.CharField(max_length=255, verbose_name='автор', **NULLABLE)
+    publishing_year = models.PositiveIntegerField(verbose_name='год публикации', **NULLABLE)
+    isbn = models.CharField(max_length=17, unique=True, **NULLABLE)
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return f"{self.name} ({self.autor}, {self.publishing_year})"
