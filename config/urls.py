b4f15a4ade387d2b6726288d53ae893e50504cbc
Vagner Elena <dipnoe@gmail.com>
2023-11-28 14:58:47 +0700

    feature(urls): add urls for users

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('book.urls', namespace='library'))
+    path('book/', include('book.urls', namespace='book')),
+    path('users/', include('users.urls', namespace='users'))
 ]
