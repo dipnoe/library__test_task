71c3dc7e5bcd74153aaf4132788316ca9b09de10
Vagner Elena <dipnoe@gmail.com>
2023-11-29 22:49:01 +0700

    feature: override post func in UserCreateAPIView to call send_welcome_message func asynchronously

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 
 from users.models import User
 from users.serializers import UserSerializer
+from users.tasks import send_welcome_message
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -12,3 +15,15 @@ class UserCreateAPIView(CreateAPIView):
     """
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                user = serializer.save()
+                send_welcome_message.delay(user.email)
+                return Response(status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({'result': f"Возникла ошибка {e}"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
