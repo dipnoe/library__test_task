c9ff3358101614179d8b148f8add78d151898184
Vagner Elena <dipnoe@gmail.com>
2023-12-11 21:34:04 +0700

    fix: remove collectstatic command and rights for nonroot user

--- docker/python/start.sh
+++ docker/python/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 if [ "$MODE" != "CELERY" ]
-then
-  ./manage.py collectstatic
-  ./manage.py migrate &&
+then ./manage.py migrate &&
   gunicorn src.config.wsgi:application --bind 0.0.0.0:8000
 fi
